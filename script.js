cc13642b805ea209b2345fd6f0073080f7fb5d9e
Prateek1771 <hitliprateek5@gmail.com>
2024-11-03 18:10:03 +0530

    simple to do

---
+++ script.js
@@ -0,0 +1,39 @@
+const input = document.getElementById('input-box')
+const container = document.getElementById('list-items')
+
+function addTask() {
+    if(input.value === ''){
+        alert('Please add task!')
+    }
+    else{
+        let li = document.createElement('li');
+        li.innerHTML = input.value;
+        container.appendChild(li)
+        let span = document.createElement('span');
+        span.innerHTML = '\u00d7';
+        li.appendChild(span);
+    }
+    input.value = '';
+    saveData()
+}
+
+container.addEventListener('click',function(e){
+    if(e.target.tagName === 'LI'){
+        e.target.classList.toggle('checked');
+        saveData()
+    }
+    else if(e.target.tagName === 'SPAN'){
+        e.target.parentElement.remove();
+        saveData()
+    }
+}, false)
+
+function saveData() {
+    localStorage.setItem('data', container.innerHTML)
+}
+
+function showTask() {
+    container.innerHTML = localStorage.getItem('data')
+}
+
+showTask()
